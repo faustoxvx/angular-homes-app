902085fc059bf1b9cad8c003b0587caaecc16f91
Fausto Nunes <faustornunes@gmail.com>
2023-09-25 18:26:07 +0100

    Initial commit
    Add Home component
    Add HousingLocation component
    Add HousingLocation interface

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {HousingLocation} from "../housing-location";
+
+@Component({
+  selector: "app-housing-location",
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading" []></h2>
+      <p class="listing-location"></p>
+    </section>
+  `,
+  styleUrls: ["./housing-location.component.css"],
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
